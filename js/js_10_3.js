66337fde6f19fc8866da7cdce514084bd1e02b48
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-26 13:08:43 +0200

    Update 10.3

--- js/js_10_3.js
+++ js/js_10_3.js
@@ -1,16 +1,20 @@
-$(function(){
-	// this code will execute after the DOM is loaded
+$(function() {
+  var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 3000);
+
+  function changeSlide() {
+    carouselList.animate({
+      'marginLeft': -400
+    }, 500, moveFirstSlide);
+  }
+
+  function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+        lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({
+      marginLeft: 0
+    });
+  }
 });
-var carouselList = $("#carousel ul");
-setInterval(changeSlide, 3000);
-function changeSlide() {
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	}
-}
-changeSlide();
-moveFirstSlide();
+
